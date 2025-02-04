d84f211a1e3b58b29e875adc58f111507a3a6f40
Matheus Souza <matheusslg1@gmail.com>
2025-02-04 18:10:49 -0300

    style: configure initial theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    primary: "#6200ea",
+    secondary: "#03dac6",
+    background: {
+      light: "#f5f5f5",
+      dark: "#242424"
+    },
+    text: {
+      light: "#faf9ec",
+      dark: "#333"
+    },
+    white: "#fff",
+  },
+  fonts: {
+    main: "'Roboto', sans-serif",
+  },
+};
+
+export type ThemeType = typeof theme;
