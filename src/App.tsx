3b5cf46bbfc049585a98417533a521dd9fa51275
Matheus Souza <matheusslg1@gmail.com>
2025-02-04 23:25:56 -0300

    chore: configure @radix-ui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Theme } from '@radix-ui/themes'
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import routes from './routes/index.routes'
@@ -6,7 +7,9 @@ import { theme } from './theme/theme'
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <RouterProvider router={routes} />
+      <Theme>
+        <RouterProvider router={routes} />
+      </Theme>
     </ThemeProvider>
   )
 }
